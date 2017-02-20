fd57c0565764d96f401b889c75300da13f43a114
Hongyang Gao <hongyang.gao@wsu.edu>
2017-02-19 22:22:22 -0800

    fix model

--- seg_cremi/newmodel/predict.sh
+++ seg_cremi/newmodel/predict.sh
@@ -1,24 +1,9 @@
-caffe_path=../../caffe3d/.build_release/tools
 GLOG_logtostderr=1 
-time $caffe_path/predict_seg_new.bin \
+$caffe_path/predict_seg_new.bin \
     --model=deploy.prototxt \
-    --data=/tempspace/tzeng/cremi/data/val/cremi3D_A_seg_valid_v1.h5 \
+    --data={cremi}_valid.h5 \
     --weights=trained_weights/inception_fcn_train_iter_33000.caffemodel \
     --predict=results/cremi_test_A.h5 \
     --shift_axis=2 \
     --shift_stride=1
-#$caffe_path/predict_seg_new.bin \
-#    --model=deploy.prototxt \
-#    --data=/tempspace/tzeng/cremi/data/val/cremi3D_B_seg_valid_v1.h5 \
-#    --weights=trained_weights/inception_fcn_train_iter_30000.caffemodel \
-#    --predict=results/cremi_test_B.h5 \
-#    --shift_axis=2 \
-#    --shift_stride=1
-#$caffe_path/predict_seg_new.bin \
-#    --model=deploy.prototxt \
-#    --data=/tempspace/tzeng/cremi/data/val/cremi3D_C_seg_valid_v1.h5 \
-#    --weights=trained_weights/inception_fcn_train_iter_30000.caffemodel \
-#    --predict=results/cremi_test_C.h5 \
-#    --shift_axis=2 \
-#    --shift_stride=1
 python predict.py > predict.log
--- seg_cremi/newmodel/train.sh
+++ seg_cremi/newmodel/train.sh
@@ -1,8 +1,6 @@
 #0!/bin/bash
-caffe_path=../../caffe3d/.build_release/tools
 $caffe_path/caffe.bin train \
     --solver=solver.prototxt \
     --gpu 1 \
     2>&1 | tee train.log
     $@
-#--snapshot=trained_weights/inception_fcn_train_iter_30000.solverstate \
